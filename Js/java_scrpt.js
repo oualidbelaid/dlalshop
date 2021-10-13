f1c3e7033e4b1b55fe3d843b4c90876c7b57eaad
Oualid_Belaid <78535330+oualidbelaid@users.noreply.github.com>
2021-10-13 10:59:40 +0100

    Update java_scrpt.js

--- Js/java_scrpt.js
+++ Js/java_scrpt.js
@@ -19,3 +19,25 @@ icon_show_menu.onclick = function () {
 }
 
 /*end main_navigateur*/
+
+/*start the main articles*/
+var list_artcl = document.getElementById("list_artcl");
+
+
+for (let i = 0; i < list_artcl.children.length; i++) {
+    list_artcl.children[i].querySelector(".fa-heart").onclick = function () {
+
+        var nbr_jaime = this.querySelector('span').textContent
+        nbr_jaime = parseFloat(nbr_jaime)
+        if (this.classList[this.classList.length - 1] == "i_actv") {
+            nbr_jaime--;
+        }
+        else {
+            nbr_jaime++;
+        }
+        this.querySelector('span').textContent = nbr_jaime
+        this.classList.toggle("i_actv")
+
+    }
+}
+/*end the main articles*/
